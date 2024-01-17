90b3b09dd71425f5d05bd1dfa928e0ca192b1af7
Timm Schoenborn <timmschoenborn@gmail.com>
2024-01-17 16:10:56 -0500

    adds toys urls models views and admin

--- main_app/urls.py
+++ main_app/urls.py
@@ -20,4 +20,11 @@ urlpatterns = [
     path('cats/<int:pk>/update', views.CatUpdate.as_view(), name='cats_update'),
     path('cats/<int:pk>/delete', views.CatDelete.as_view(), name='cats_delete'),
     path('cats/<int:cat_id>/add_feeding', views.add_feeding, name='add_feeding'),
+    # toy urls
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+    # once this is all set up correctly, we'll add some code to associate our toys with our cats, as well as a url to unassociate toys and cats
 ]
